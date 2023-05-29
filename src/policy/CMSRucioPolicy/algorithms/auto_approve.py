9574a73f5fc9ac2e9de625cfe8a0e3b6c130aeef
Rahul Chauhan <rahul.chauhan@cern.ch>
2023-05-30 01:31:58 +0200

    Adding first auto_approve policy

---
+++ src/policy/CMSRucioPolicy/algorithms/auto_approve.py
@@ -0,0 +1,95 @@
+"""
+Auto approve algorithm for CMS Rucio policy
+"""
+
+from rucio.core.account import has_account_attribute
+from rucio.core.rule import list_rules
+from rucio.core.did import list_files
+import re
+
+GLOBAL_USAGE_THRESHOLD = 1e15
+
+
+def global_approval(did, rule_attributes, session):
+    """
+    Auto approves rules by users that satisfy the following conditions:
+    - Activity is Data Brokering
+        A use of separate activity will help us utilise the current monitoring 
+        without needing extra effort
+    - User is under the global usage threshold
+    - User is not blacklisted
+    - Rule lifetime is less than a month
+    - Rule is not locked (Locked rules do not get deleted until they are unlocked)
+    - RSE expression belongs to the set of expressions that are allowed to be used by the user
+        This helps us taint RSEs in various sets without needing to change code
+        for e.g. one could taint all primary sites with data_brokering_rse_set=1
+
+
+    :param did: The DID being approved
+    :type did dict: {
+        'scope': 'cms|user.username|group.groupname',
+        'name': 'lfn',
+        'account': 'account which registered the DID',
+        'did_type': 'FILE, DATASET, CONTAINER',
+        'is_open': 'True | False',
+        'bytes': 'size in bytes (NULL for a CONTAINER)',
+        }
+
+    :param rule_attributes: The attributes of the rule being approved  
+    :type rule_attributes dict: {
+        'account': account,
+        'lifetime': lifetime,
+        'rse_expression': rse_expression,
+        'copies': copies,
+        'weight': weight,
+        'grouping': grouping,
+        'locked': locked,
+        'comment': comment,
+        'meta': meta,
+        'ignore_availability': ignore_availability,
+        } 
+    :param session: The database session in use
+
+    :returns: True if the rule should be auto approved, False otherwise
+    """
+
+    account = rule_attributes['account']
+
+    # Check if the account is blacklisted
+    if has_account_attribute(account, 'rule_blacklisted', session=session):
+        return False
+
+    # Check activity is Data Brokering
+    if rule_attributes['activity'] != 'Data Brokering':
+        return False
+
+    # Check if the rule is locked
+    if rule_attributes['locked']:
+        return False
+
+    # Check if the rule lifetime is less than a month
+    if rule_attributes['lifetime'] > 2592000:
+        return False
+
+    # Check if the RSE expression is allowed for the user
+    # rse_expression should be of the form "data_brokering_rse_set=<number>"
+    other_valid_rse_expressions = ['cms_type=real&rse_type=DISK']
+    rse_expression = rule_attributes['rse_expression']
+    if not re.match(r'^data_brokering_rse_set=\d+$', rse_expression) and rse_expression not in other_valid_rse_expressions:
+        return False
+
+    # Check global usage of the account under this activity
+    data_brokering_usage = 0
+    data_brokering_rules = list_rules(filters={'account': account, 'activity': 'Data Brokering'}, session=session)
+    for db_rule in data_brokering_rules:
+        scope = db_rule['scope']
+        name = db_rule['name']
+        rule_files = list_files(scope, name, session=session)
+        rule_size = sum([file['bytes'] for file in rule_files])
+        data_brokering_usage += rule_size
+
+    this_rule_size = sum([file['bytes'] for file in list_files(did['scope'], did['name'], session=session)])
+    if data_brokering_usage + this_rule_size > GLOBAL_USAGE_THRESHOLD:
+        return False
+
+    return True
